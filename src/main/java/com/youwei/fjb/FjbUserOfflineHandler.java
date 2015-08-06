a783b1439083b9490648291ccd4943874d5fe7c8
runningship <runningship@gmail.com>
2015-08-06 09:18:22 +0800

    添加城市信息

--- src/main/java/com/youwei/fjb/FjbUserOfflineHandler.java
+++ src/main/java/com/youwei/fjb/FjbUserOfflineHandler.java
@@ -15,15 +15,16 @@ public class FjbUserOfflineHandler implements UserOfflineHandler{
 	public void handle(HttpServletRequest req, ServletResponse response) {
 		try {
 			HttpServletResponse resp = (HttpServletResponse)response;
+			resp.setHeader("userOffline", "true");
 			if(req.getRequestURI().contains("admin")){
-				resp.sendRedirect(req.getServletContext().getContextPath()+"/admin/login.jsp");
-//				response.getWriter().write("<script type='text/javascript'>window.top.location='"+req.getServletContext().getContextPath()+"/admin/login.jsp'</script>");
+//				resp.sendRedirect(req.getServletContext().getContextPath()+"/admin/login.jsp");
+				response.getWriter().write("<script type='text/javascript'>window.top.location='"+req.getServletContext().getContextPath()+"/admin/login.jsp'</script>");
 			}else if(req.getRequestURI().contains("/m/")){
-				resp.sendRedirect(req.getServletContext().getContextPath()+"/m/login.jsp");
-//				response.getWriter().write("<script type='text/javascript'>window.top.location='"+req.getServletContext().getContextPath()+"/m/login.jsp'</script>");
+//				resp.sendRedirect(req.getServletContext().getContextPath()+"/m/login.jsp");
+				response.getWriter().write("<script type='text/javascript'>window.top.location='"+req.getServletContext().getContextPath()+"/m/login.jsp'</script>");
 			}else{
-				resp.sendRedirect(req.getServletContext().getContextPath()+"/login.jsp");
-//				response.getWriter().write("<script type='text/javascript'>window.top.location='"+req.getServletContext().getContextPath()+"/login.jsp'</script>");
+//				resp.sendRedirect(req.getServletContext().getContextPath()+"/login.jsp");
+				response.getWriter().write("<script type='text/javascript'>window.top.location='"+req.getServletContext().getContextPath()+"/login.jsp'</script>");
 			}
 			
 		} catch (IOException e) {
